af9c11d5e16ed6a888967c47e7d5bf59ddae5013
Yueh-Cheng Liu <yuehcheng.liu@tum.de>
2023-10-11 22:50:38 +0200

    Extract common utils

--- iphone/prepare_iphone_data.py
+++ iphone/prepare_iphone_data.py
@@ -21,42 +21,8 @@ import imageio as iio
 import lz4.block
 
 from common.scene_release import ScannetppScene_Release
+from common.utils.utils import run_command, load_yaml_munch, load_json, read_txt_list
 
-def read_txt_list(path):
-    with open(path) as f: 
-        lines = f.read().splitlines()
-
-    return lines
-
-def load_json(path):
-    with open(path) as f:
-        j = json.load(f)
-
-    return j
-
-def load_yaml_munch(path):
-    with open(path) as f:
-        y = yaml.load(f, Loader=yaml.Loader)
-
-    return Munch.fromDict(y)
-
-def run_command(cmd: str, verbose=False):
-    """Runs a command and returns the output.
-
-    Args:
-        cmd: Command to run.
-        verbose: If True, logs the output of the command.
-    Returns:
-        The output of the command if return_output is True, otherwise None.
-    """
-    out = subprocess.run(cmd, capture_output=not verbose, shell=True, check=False)
-    if out.returncode != 0:
-        if out.stderr is not None:
-            print(out.stderr.decode("utf-8"))
-        sys.exit(1)
-    if out.stdout is not None:
-        return out.stdout.decode("utf-8")
-    return out
 
 def extract_rgb(scene):
     scene.iphone_rgb_dir.mkdir(parents=True, exist_ok=True)
