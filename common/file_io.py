2ed5fce7b69ae69a6cbfaa0b5ff3783d1778d486
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2023-10-26 12:01:15 +0200

    Add load json and load yaml to file io

--- common/file_io.py
+++ common/file_io.py
@@ -2,6 +2,21 @@
 
 import yaml
 from munch import Munch
+import json
+import yaml
+
+def load_json(path):
+    with open(path) as f:
+        j = json.load(f)
+
+    return j
+
+def load_yaml(path):
+    with open(path) as f:
+        y = yaml.load(f, Loader=yaml.Loader)
+
+    return y
+
 
 def read_txt_list(path):
     with open(path) as f: 
