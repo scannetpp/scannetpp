d4463dee8a8ed7866a87ce6fef6d7d595f91d71f
Chandan Yeshwanth <chandan.yeshwanth@tum.de>
2024-11-23 18:33:07 +0100

    Add get undistort maps

--- common/utils/dslr.py
+++ common/utils/dslr.py
@@ -1,6 +1,12 @@
 import cv2
 import numpy as np
 
+def get_undistort_maps(intrinsic, distort_params, undistort_intrinsic, img_height, img_width):
+    undistort_map1, undistort_map2 = cv2.fisheye.initUndistortRectifyMap(
+            intrinsic, distort_params, np.eye(3), undistort_intrinsic, (img_width, img_height), cv2.CV_32FC1
+        )
+    return undistort_map1, undistort_map2
+
 def compute_undistort_intrinsic(K, height, width, distortion_params):
     assert len(distortion_params.shape) == 1
     assert distortion_params.shape[0] == 4  # OPENCV_FISHEYE has k1, k2, k3, k4
